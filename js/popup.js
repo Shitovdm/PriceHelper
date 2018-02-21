443ce859cb5a126b41b2c598908b11cdee95b2db
Shitov Dmitry <shitov.dm@gmail.com>
2018-02-21 16:59:17 +0300

    Первый рабочий вариан расширения. Выводит исходную страницу в Steam открытого предмета в cs go tm.

--- js/popup.js
+++ js/popup.js
@@ -16,8 +16,6 @@ $(document).ready(function(){
                 state:false, 
                 desc:"" 
             };   
-            $("#tmPrice").append(res);
-            //port.postMessage(res);
             //  Парсим название предмета.
             var NameregEx = /<h1.*?>.*?<\/h1>/ig;
             var nameContainer = NameregEx.exec(res);
@@ -73,8 +71,8 @@ $(document).ready(function(){
                 var marketNameURL = marketName.replace( /\|/g , "%7C" );
                 marketNameURL = marketNameURL.replace( /\s/g,"%20" );
                 var URL = marketNameURL + "%20%28" + Exterior + "%29";
-                
                 port.postMessage(URL);
+                getPageContent(URL);
             }else{  //  Ошибка.
                 port.postMessage(Error.desc);
             }
@@ -82,50 +80,31 @@ $(document).ready(function(){
             
         }   
         
-        chrome.tabs.executeScript(null, {
+        chrome.tabs.executeScript(null, {   //  При нажатии на иконку расширения происходит парсинг имени предмета.
             code: 'var testElements = document.getElementsByClassName("item-h1");' +
                 ' var el = testElements[0]; ' +
-                ' ' +
-                ' if (el) {el.style.color = "red";}' +
                 ' el.innerHTML'
         }, function (results) {
             handler(results);
         });
         
-        
-        /*chrome.tabs.executeScript({
-            code: 'var testElements = document.getElementsByClassName("ip-bestprice");'+
-            ' var el = testElements[0]; '+
-            ' console.log(el.innerHTML);'+
-            ' $("#tmPrice").append(el.innerHTML);'+
-            ' if (el) {el.style.color = "red";}'
-        });*/
-        
-        //  Составляем url страницы.
-        var text = '<h1 style="color: #000000">CZ75-Auto | Polymer</h1>';
-        var regEx = /<h1.*?>.*?<\/h1>/ig;
-        var match = regEx.exec(text);
-        var matchParse = match[0].substring(27).substring(0,match[0].length - 5);
-        
-        var matchReplace = matchParse.replace( /\|/g , "%7C" );  //    AK-47%20%7C%20Redline%20%28Field-Tested%29
-        matchReplace = matchReplace.replace(/\s/g,"%20");
-        //matchReplace = matchReplace.replace(/\(/g,"%28");
-        //matchReplace = matchReplace.replace(/\)/g,"%29");
-        var url = "http://steamcommunity.com/market/listings/730/" + matchReplace; 
-        
-        $("#tmPrice").append(matchReplace);
-        /*
-         * // Получаем и парсим код Steam страницы с предметом
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29", true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                $("#tmPrice").append(xhr.responseText);
-                //  Парсим код страницы.
-            }
+        //  Функция получает контент страницы Steam.
+        function getPageContent(urlItem){
+            var url = "http://steamcommunity.com/market/listings/730/" + urlItem;
+            // Получаем и парсим код Steam страницы с предметом
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    $("#tmPrice").append(xhr.responseText);
+                    //  Парсим код страницы.
+                }
+            };
+            xhr.send();
         }
-        xhr.send();
-        */
+        
+        
+        
     //};
 });
 
